08bae74ed8607c2a71feef86bc6b24e0e1b55a7e
Rohan Gandhi <rohangandhi109@gmail.com>
2021-01-08 12:05:14 -0500

    final

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -2,14 +2,17 @@ module.exports = (req, res, next) => {
     if (!req.session.isLoggedIn) {
         return res.redirect('/login');
     }
-    if (!req.session.user.admin != 'true') {
-        console.log(req.session.user)
-        console.log(req.session.user.admin)
-        res.status(401).render('401', {
-            pageTitle: 'Unauthorized',
-            path: '/401',
-            isAuthenticated: req.session.isLoggedIn
+    bcrypt
+        .compare(req.session.user.admin,)
+        .then((doMatch) => {
+            if (!doMatch) {
+                res.status(401).render('401', {
+                    pageTitle: 'Unauthorized',
+                    path: '/401',
+                    isAuthenticated: req.session.isLoggedIn
+                });
+            }
         });
-    }
+
     next();
 }
