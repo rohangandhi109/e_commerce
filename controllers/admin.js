781bd6b9a254f80720aae1a70379b7a27b81f31a
Rohan Gandhi <rohangandhi109@gmail.com>
2021-01-08 10:51:05 -0500

    final

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 const Product = require('../models/product');
 const fileHelper = require('../util/file');
