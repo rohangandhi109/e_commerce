d726df09f22f8ed881c65805f69c48d4347ebf93
Rohan Gandhi <rohangandhi109@gmail.com>
2021-01-08 11:50:49 -0500

    final

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -3,6 +3,8 @@ module.exports = (req, res, next) => {
         return res.redirect('/login');
     }
     if (!req.session.user.admin != 'true') {
+        console.log(req.session.user)
+        console.log(req.session.user.admin)
         res.status(401).json({ message: 'You are not authorized' });
     }
     next();
